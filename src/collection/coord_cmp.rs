bcd7cbd7d3e1d78d5a6b6ce6ee9e0c076ab861e2
kena0ki <ken3574.25@gmail.com>
2022-06-19 19:32:45 +0900

    coor_cmp

--- src/collection/coord_cmp.rs
+++ src/collection/coord_cmp.rs
@@ -1,22 +1,11 @@
-use std::collections::{BTreeMap,BTreeSet};
-pub fn coord_cmp<T:Ord+Clone+Copy>(a: &Vec<T>)
-    -> (Vec<usize>, BTreeMap<T,usize>, BTreeMap<usize,T>, usize) {
-    let mut set = BTreeSet::<T>::new();
-    for i in 0..a.len() {
-        set.insert(a[i]);
-    }
-    let mut size = 0;
-    let mut to_id = BTreeMap::<T,usize>::new();
-    let mut to_val = BTreeMap::<usize,T>::new();
-    for val in set {
-        to_id.insert(val, size);
-        to_val.insert(size, val);
-        size+=1;
-    }
-    let mut compressed = vec![0; a.len()];
-    for i in 0..a.len() {
-        compressed[i] = *to_id.get(&a[i]).unwrap();
-    }
+use std::collections::BTreeMap;
+pub fn coord_cmp<T:Ord+Clone+Copy>(va: &Vec<T>) -> (Vec<usize>, BTreeMap<T,usize>, Vec<T>, usize) {
+    let mut to_val = va.clone();
+    to_val.sort_unstable();
+    to_val.dedup();
+    let to_id:BTreeMap<T,usize> = to_val.iter().copied().enumerate().map(|(i,v)| (v,i)).collect();
+    let size = to_val.len();
+    let compressed = va.iter().map(|a| to_id[&a]).collect();
     return (compressed, to_id, to_val, size);
 }
 
@@ -25,8 +14,8 @@ mod test {
     use super::*;
     #[test]
     fn test_coord_comp() {
-        let mut coords = vec![16, 99, 45, 18];
+        let mut coords = vec![16, 99, 45, 18, 45];
         let comp = coord_cmp(&mut coords);
-        assert_eq!(vec![0,3,2,1], comp.0);
+        assert_eq!(vec![0,3,2,1,2], comp.0);
     }
 }
